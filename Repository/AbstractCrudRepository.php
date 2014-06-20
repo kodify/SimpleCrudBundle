67916663f24d0e268572a1069cfad3d136269e8f
Adria Cidre <adria.cidre@gmail.com>
2014-06-20 11:20:19 +0200

    Inner and Left join on same query

--- Repository/AbstractCrudRepository.php
+++ Repository/AbstractCrudRepository.php
@@ -52,6 +52,10 @@ abstract class AbstractCrudRepository extends EntityRepository
             $query->setMaxResults($pageSize)
                 ->setFirstResult($currentPage * $pageSize);
 
+            foreach ($this->selectInnerJoin as $join) {
+                $query->innerJoin($join['field'], $join['alias']);
+            }
+
             foreach ($this->selectLeftJoin as $join) {
                 $query->leftJoin($join['field'], $join['alias']);
             }
@@ -126,6 +130,7 @@ abstract class AbstractCrudRepository extends EntityRepository
 
         if (is_array($this->selectLeftJoin)) {
             $this->getQueryForSelectLeftJoin($filters, $pageSize, $currentPage, $sort, $defaultSort, $query);
+            Parser\FilterParser::parseFilters($filters, $query);
         } else {
             if (is_array($this->selectInnerJoin)) {
                 $this->getQueryForSelectInnerJoin($filters, $pageSize, $currentPage, $sort, $defaultSort, $query);
@@ -176,6 +181,11 @@ abstract class AbstractCrudRepository extends EntityRepository
             ->select('p.' . $identifiers[0])
             ->setMaxResults($pageSize)
             ->setFirstResult($currentPage * $pageSize);
+
+        foreach ($this->selectInnerJoin as $join) {
+            $query->innerJoin($join['field'], $join['alias']);
+            $queryToRetrieveIds->innerJoin($join['field'], $join['alias']);
+        }
 
         foreach ($this->selectLeftJoin as $join) {
             $query->leftJoin($join['field'], $join['alias']);
